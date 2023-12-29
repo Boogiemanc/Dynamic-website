fa456d9eef358d989cb5b01083c389605a432462
Desiree Pun Ching Yan <yanp2@cardiff.ac.uk>
2023-12-29 15:56:50 +0000

    commit

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0,"/root/customer-account-automation/")
+from JinjaServer import app as application
+if __name__ == "__main__":
+ app = application
