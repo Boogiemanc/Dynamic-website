9a5ca751acecbd6c93ff9f23239aa7879a1046cc
Desiree Pun Ching Yan <yanp2@cardiff.ac.uk>
2024-01-09 03:23:59 +0000

    commit

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,3 @@
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/root/customer-account-automation/")
+
+
 from app import app as application
-if __name__ == "__main__":
- app = application
